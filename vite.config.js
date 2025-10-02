5fbf642417ef54e954f49e8431155bc67ede82b5
Danil Lunyakin <lunyakindanya@gmail.com>
2025-10-02 20:23:26 +0300

    fix: add redirects for Netlify CMS

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
+// import includeHtml from "vite-plugin-include-html";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [includeHtml()]
+// });
+
+
 import includeHtml from "vite-plugin-include-html";
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { copyFileSync } from "fs";
 
 export default defineConfig({
-  plugins: [includeHtml()]
+  plugins: [
+    includeHtml(),
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        copyFileSync(
+          resolve(__dirname, "public/_redirects"),
+          resolve(__dirname, "dist/_redirects")
+        );
+      },
+    },
+  ],
 });
+
+
 
 // import { defineConfig } from 'vite';
 
